f438b3ad6f1aa0db2c2a1260a0c93a06b4a427e1
MaILW <55791541+MaILW@users.noreply.github.com>
2019-10-06 16:20:10 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Бинарный_поиск
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                int col = Sort.vvod();
+                if (col == -1)
+                {
+                    Console.WriteLine("Для выхода нажмите q. Для продолжения любую кнопку.");
+                    continue;
+                }
+             
+                int[] arr = new int[col];
+                Random rnd = new Random();
+                for (int i = 0; i < col; i++)
+                {
+                    arr[i] = rnd.Next(0, 100);
+                }
+
+                Console.WriteLine("Исходный массив:");
+                Sort.vivod(arr);
+                Sort.sort1(arr);
+                do
+                {
+                    Console.WriteLine("\n\n\nОтсортированный массив:");
+                    Sort.vivod(arr);
+                    Console.Write("Ввести элемент поиска: ");
+                    int target = Convert.ToInt32(Console.ReadLine());
+                    int k = Sort.binPoisk(target, arr);
+                    Console.WriteLine("Бинарный поиск:");
+                    if (k > -1)
+                        Console.WriteLine("Номер элемента = {0}", k);
+                    else
+                        Console.WriteLine("нет заданного элемента");
+
+                    Console.WriteLine("Для выхода нажмите q. Для продолжения любую кнопку.");
+                }while(Console.ReadLine() != "q");
+            } while (Console.ReadLine() != "q");
+            
+       }
+    }
+}
---
+++ Sort.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Бинарный_поиск
+{
+    class Sort
+    {
+        static public int[] sort1(int[] array)
+        {
+            for (int j = 0; j < array.Length  -1; j++)
+            {
+               for (int i = 0; i < array.Length - 1; i++)
+                 {
+                     int x;
+                    if (array[i] > array[i + 1])
+                    {
+                        x = array[i]; array[i] = array[i + 1]; array[i + 1] = x;
+                    }
+
+
+
+               }
+            }
+            return array;
+        }
+
+        static public int binPoisk(int target, int[] array)
+        {
+            int mid;
+            int left = 0;
+            int right = array.Length - 1;
+            mid = (right + left) / 2;
+            while (left < right - 1)
+            {
+                mid = (right + left) / 2;
+                if (array[mid] == target)
+                    return mid;
+                if (array[mid] < target)
+                    left = mid;
+                else
+                    right = mid;
+            }
+            if (array[mid] != target)
+            {
+                if (array[left] == target)
+                    mid = left;
+                else
+                {
+                    if (array[right] == target)
+                        mid = right;
+                    else
+                        mid = -1;
+                };
+            }
+            return mid;
+        }
+        static public void vivod(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+        }
+        static public int vvod()
+        {
+            int k;
+            Console.Write("Ведите число элементов массива: ");
+            //k = Console.ReadLine();
+            
+            try 
+            { 
+               k =  Convert.ToInt32(Console.ReadLine()); 
+            }
+            //if (Convert.ToInt32(k) == 0) 
+           
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Неверный формат данных\n");
+                return -1;
+            }
+            //k = Convert.ToInt32(Console.ReadLine());
+            //if (k == -2) Console.WriteLine("Неверный формат данных");
+            
+            if (k < 0) 
+            {
+                Console.WriteLine("Отрицательное значение не допустимо\n");
+                
+                return -1; 
+            }
+            return k;
+        }
+    }
+}
+
